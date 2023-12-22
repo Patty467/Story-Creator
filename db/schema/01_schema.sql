db4013665bbd5305e96875554d60c9a4a8fcc779
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-12-22 10:51:14 -0800

    update schema

--- db/schema/01_schema.sql
+++ db/schema/01_schema.sql
@@ -1,25 +1,31 @@
-DROP TABLE IF EXISTS "users" CASCADE;
-CREATE TABLE "users" (
-  "id" SERIAL PRIMARY KEY NOT NULL,
-  "name" VARCHAR(255) NOT NULL
+-- Create the users table
+DROP TABLE IF EXISTS users CASCADE;
+
+CREATE TABLE users(
+  id serial PRIMARY KEY NOT NULL,
+  name varchar(255) NOT NULL
 );
 
-DROP TABLE IF EXISTS "stories" CASCADE;
-CREATE TABLE "stories" (
-  "id" SERIAL PRIMARY KEY NOT NULL,
-  "title" VARCHAR(255) NOT NULL,
-  "content" TEXT,
-  "completed" BOOLEAN NOT NULL DEFAULT TRUE,
-  "users_id" INTEGER
+-- Create the stories table with a foreign key reference to the users table
+DROP TABLE IF EXISTS stories CASCADE;
+
+CREATE TABLE stories(
+  id serial PRIMARY KEY NOT NULL,
+  title varchar(255) NOT NULL,
+  content text,
+  completed boolean NOT NULL DEFAULT TRUE,
+  users_id int REFERENCES users(id)
 );
 
-DROP TABLE IF EXISTS "submissions" CASCADE;
-CREATE TABLE "submissions" (
-  "id" SERIAL PRIMARY KEY NOT NULL,
-  "content" TEXT,
-  "votes" INTEGER,
-  "active" BOOLEAN NOT NULL DEFAULT TRUE,
-  "accepted" BOOLEAN NOT NULL DEFAULT TRUE,
-  "users_id" INTEGER,
-  "stories_id" INTEGER
+-- Create the submissions table with foreign key references to users and stories
+DROP TABLE IF EXISTS submissions CASCADE;
+
+CREATE TABLE submissions(
+  id serial PRIMARY KEY NOT NULL,
+  content text,
+  votes integer,
+  active boolean NOT NULL DEFAULT TRUE,
+  accepted boolean NOT NULL DEFAULT TRUE,
+  users_id integer REFERENCES users(id),
+  stories_id integer REFERENCES stories(id)
 );
