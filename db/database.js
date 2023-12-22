71c87a8d8729eef98297768715df83426c6a8663
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-12-22 10:53:26 -0800

    initial commit

---
+++ db/database.js
@@ -0,0 +1,25 @@
+const db = require('./connection');
+
+//get all stories
+const getAllStories = () => {
+  return db.query(`
+  SELECT stories.*, users.*
+  FROM stories
+  JOIN users ON stories.users_id = users.id;
+  `)
+    .then(data => {
+      return data.rows;
+    });
+};
+
+//get specific story
+const getStory = (storyId) => {
+  return db.query(`
+  SELECT stories.* users.*
+  FROM stories
+  JOIN users ON stories.users_id = users.id
+  WHERE stories.id = $1;
+  `, [storyId])
+};
+
+module.exports = { getAllStories, getStory };
