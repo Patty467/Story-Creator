2985e487d8ddacf85d59f440baef04713bebaaf1
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-12-22 10:52:31 -0800

    initial commit

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,6 +1,6 @@
-INSERT INTO users 
-  (id, name) VALUES (1, 'Alice');
-  (id, name) VALUES (2, 'Jack');
-  (id, name) VALUES (3, 'Boris');
-  (id, name) VALUES (4, 'John');
-  (id, name) VALUES (5, 'Candice');
+INSERT INTO users(id, name)
+  VALUES (1, 'Alice'),
+(2, 'Jack'),
+(3, 'Boris'),
+(4, 'John'),
+(5, 'Candice');
--- db/seeds/02_stories.sql
+++ db/seeds/02_stories.sql
@@ -1,155 +1,95 @@
-INSERT INTO stories 
-
+INSERT INTO stories(id, title, content, completed, users_id)
 -- Uncompleted Stories
-
-  (id, title, content, completed, uers_id) 
-  VALUES (
-  1, 
-  'Harmonic Dust', 
-  'The parched earth groaned under a relentless sun, the horizon shimmering like a mirage above the cracked and dusty plains. 
-  A lone rider, silhouetted against the dying embers of the day, cut a lonely figure. 
-  His weathered Stetson sat low over eyes that held the weariness of a thousand sun-baked miles, 
-  his chapped lips pursed around a harmonica that whined a mournful tune of lost hopes and empty promises. 
-  This was no Sunday stroll through fields of sunflowers; this was a man riding into the teeth of a dust storm, 
-  a storm brewing not just in the sky, but in the depths of his soul.', 
-  false,
-  1
-  );
-  
-  (id, title, content, completed, users_id) 
-  VALUES (
-  2, 
-  'Through the Skylight', 
-  'Rain pummeled the corrugated metal roof of the gym, drumming a chaotic rhythm against the echoes of squeaking sneakers and grunts of exertion. 
-  Maya dribbled with steely focus, the worn orange basketball an extension of her pulse, weaving through a gauntlet of towering shadows in crimson jerseys. 
-  Beads of sweat stung her eyes, blending with the rain that leaked through the skylights, turning the court into a slick battlefield. 
-  Today, the state championship wasnt just a trophy; it was her escape ticket, 
-  a one-way pass out of this dead-end town and into a future as bright and boundless as the sky before the storm. 
-  Her heart pounded, a counterpoint to the rains relentless roar, each beat a promise and a prayer – this dribble, this pass, this shot, 
-  anything to break free from the gravity of her small-town dreams.', 
-  false,
-  1
-  );
-
-  (id, title, content, completed, users_id) 
-  VALUES (
-  3, 
-  'Whinnying for Wings', 
-  'The sun, a giant orange yolk smeared across the stable ceiling, oozed its morning light onto Buttercup. 
-  She stretched, hooves tucked like dainty ballet slippers beneath her chest, and squinted at the dust motes dancing in the golden beam. 
-  But the joy of a new day, the usual thrum of anticipation for oats and playful nudges from her stablemate, Prince Charming, remained stubbornly absent. 
-  Instead, a gnawing pang, as familiar as the hay she chomped and as old as the cobwebs in the rafters, settled in her chest. 
-  "Why, oh why," she sighed, the breath ruffling her chestnut mane, "cant I just be a duck?" The thought had plagued her since she first wobbled on spindly legs, 
-  a foal yearning for the boundless sky, not the four-walled horizon of the stable. Today, 
-  with the sun whispering of open horizons and a pond shimmering below the barn window, the ache was especially acute. 
-  She longed to trade her hay for pondweed, her hooves for webbed feet, her whinny for a cheery quack. 
-  Maybe, just maybe, this could be the day she finally took flight, no wings needed, just pure, feathery desire and a pond full of possibilities.', 
-  false,
-  2
-  );
-
-  (id, title, content, completed, users_id) 
-  VALUES (
-  4, 
-  'Quantum Alphabet Soup', 
-  'A crackle of static birthed a neon butterfly from the television screen, its wings morphing into tangled constellations before melting into a puddle of pixels. 
-  Across the room, a book coughed dust as its spine unfurled, pages whispering incantations in tongues of swirling ink. 
-  Sunlight filtering through a prismatic chandelier shattered, casting rainbows onto a bowl of alphabet soup where letters writhed and morphed, 
-  forming fleeting sentences before dissolving back into broth. In this apartment, reality was a kaleidoscope, each fragment fracturing further, 
-  an endless dance of meaning unmoored from form. And nestled amongst the chaos, sat Alice, eyes wide and curious, 
-  ready to tumble down the rabbit hole into a labyrinth of her own making.', 
-  false,
-  2
-  );
-
-
--- Completed stories
-
-
-  (id, title, content, completed, users_id) 
-  VALUES (
-  5, 
-  'Duck-Duck, The Duck', 
-  'Duck-Duck, the Duck, wasn''t like the other ducks. While his buddies bobbed merrily on the pond, quacking away about worms and puddles, 
+  VALUES (1, 'Harmonic Dust', 'The parched earth groaned under a relentless sun, the horizon shimmering like a mirage above the cracked and dusty plains.
+  A lone rider, silhouetted against the dying embers of the day, cut a lonely figure.
+  His weathered Stetson sat low over eyes that held the weariness of a thousand sun-baked miles,
+  his chapped lips pursed around a harmonica that whined a mournful tune of lost hopes and empty promises.
+  This was no Sunday stroll through fields of sunflowers; this was a man riding into the teeth of a dust storm,
+  a storm brewing not just in the sky, but in the depths of his soul.', FALSE, 1),
+(2, 'Through the Skylight', 'Rain pummeled the corrugated metal roof of the gym, drumming a chaotic rhythm against the echoes of squeaking sneakers and grunts of exertion.
+  Maya dribbled with steely focus, the worn orange basketball an extension of her pulse, weaving through a gauntlet of towering shadows in crimson jerseys.
+  Beads of sweat stung her eyes, blending with the rain that leaked through the skylights, turning the court into a slick battlefield.
+  Today, the state championship wasnt just a trophy; it was her escape ticket,
+  a one-way pass out of this dead-end town and into a future as bright and boundless as the sky before the storm.
+  Her heart pounded, a counterpoint to the rains relentless roar, each beat a promise and a prayer – this dribble, this pass, this shot,
+  anything to break free from the gravity of her small-town dreams.', FALSE, 1),
+(3, 'Whinnying for Wings', 'The sun, a giant orange yolk smeared across the stable ceiling, oozed its morning light onto Buttercup.
+  She stretched, hooves tucked like dainty ballet slippers beneath her chest, and squinted at the dust motes dancing in the golden beam.
+  But the joy of a new day, the usual thrum of anticipation for oats and playful nudges from her stablemate, Prince Charming, remained stubbornly absent.
+  Instead, a gnawing pang, as familiar as the hay she chomped and as old as the cobwebs in the rafters, settled in her chest.
+  "Why, oh why," she sighed, the breath ruffling her chestnut mane, "cant I just be a duck?" The thought had plagued her since she first wobbled on spindly legs,
+  a foal yearning for the boundless sky, not the four-walled horizon of the stable. Today,
+  with the sun whispering of open horizons and a pond shimmering below the barn window, the ache was especially acute.
+  She longed to trade her hay for pondweed, her hooves for webbed feet, her whinny for a cheery quack.
+  Maybe, just maybe, this could be the day she finally took flight, no wings needed, just pure, feathery desire and a pond full of possibilities.', FALSE, 2),
+(4, 'Quantum Alphabet Soup', 'A crackle of static birthed a neon butterfly from the television screen, its wings morphing into tangled constellations before melting into a puddle of pixels.
+  Across the room, a book coughed dust as its spine unfurled, pages whispering incantations in tongues of swirling ink.
+  Sunlight filtering through a prismatic chandelier shattered, casting rainbows onto a bowl of alphabet soup where letters writhed and morphed,
+  forming fleeting sentences before dissolving back into broth. In this apartment, reality was a kaleidoscope, each fragment fracturing further,
+  an endless dance of meaning unmoored from form. And nestled amongst the chaos, sat Alice, eyes wide and curious,
+  ready to tumble down the rabbit hole into a labyrinth of her own making.', FALSE, 2),
+  -- Completed stories
+(5, 'Duck-Duck, The Duck', 'Duck-Duck, the Duck, wasn''t like the other ducks. While his buddies bobbed merrily on the pond, quacking away about worms and puddles,
   Duck-Duck dreamed of loftier things. He yearned to dance on clouds, sing with the stars, and fly beyond the horizon.
 
-  Every morning, as the sun painted the sky with gentle orange and pink, Duck-Duck would flap his tiny wings as hard as he could, 
-  hoping to catch a breeze and lift him into the air. But all he managed was a hilarious, duck-bottomed shuffle across the water, 
+  Every morning, as the sun painted the sky with gentle orange and pink, Duck-Duck would flap his tiny wings as hard as he could,
+  hoping to catch a breeze and lift him into the air. But all he managed was a hilarious, duck-bottomed shuffle across the water,
   sending ripples bouncing and his siblings into fits of giggles.
 
-  One day, a wise old goose with feathers like fluffy snow saw Duck-Duck''s persistent flapping. "Little one," he honked kindly, 
+  One day, a wise old goose with feathers like fluffy snow saw Duck-Duck''s persistent flapping. "Little one," he honked kindly,
   "you can''t fly with ordinary wings. You need wings of dreams, woven from moonlight and wishes."
 
-  Intrigued, Duck-Duck spent that night gazing at the moon, whispering his greatest dreams into the cool night air. 
-  As the first rays of dawn kissed the water, he felt a strange fluttering in his chest. He looked down and gasped. 
+  Intrigued, Duck-Duck spent that night gazing at the moon, whispering his greatest dreams into the cool night air.
+  As the first rays of dawn kissed the water, he felt a strange fluttering in his chest. He looked down and gasped.
   There, nestled against his heart, were two shimmering wings, like spun moonlight and stardust!
 
-  With a joyous quack, Duck-Duck unfurled his wings and took to the sky. He danced with the clouds, sang with the stars, 
-  and soared beyond the horizon, his dreams finally taking flight. And though he always returned to the pond, 
-  sharing stories of his adventures with his siblings, Duck-Duck, the Duck, never forgot the wise goose''s words: 
-  the most beautiful wings are the ones woven from dreams.', 
-  true,
-  1
-  );
-
-  (id, title, content, completed, users_id) 
-  VALUES (
-  6, 
-  'Concrete Jungles and Manicured Lawns: A Streetwise Prince in Bel-Air', 
-  '
-  West Philadelphia, born and raised. That''s where I spent most of my days, chillin'' out, maxin'', relaxin'' all cool. 
-  My name''s Lil'' Jazzy, streetwise and smooth, but my life wasn''t all sunshine and parkway jams. My pops, bless his soul, 
-  got caught in a jam, a bad one, the kind with sirens and orange jumpsuits. So it was Uncle Phil, big and rich, who stepped in, 
+  With a joyous quack, Duck-Duck unfurled his wings and took to the sky. He danced with the clouds, sang with the stars,
+  and soared beyond the horizon, his dreams finally taking flight. And though he always returned to the pond,
+  sharing stories of his adventures with his siblings, Duck-Duck, the Duck, never forgot the wise goose''s words:
+  the most beautiful wings are the ones woven from dreams.', TRUE, 1),
+(6, 'Concrete Jungles and Manicured Lawns: A Streetwise Prince in Bel-Air', '
+  West Philadelphia, born and raised. That''s where I spent most of my days, chillin'' out, maxin'', relaxin'' all cool.
+  My name''s Lil'' Jazzy, streetwise and smooth, but my life wasn''t all sunshine and parkway jams. My pops, bless his soul,
+  got caught in a jam, a bad one, the kind with sirens and orange jumpsuits. So it was Uncle Phil, big and rich, who stepped in,
   whisking me away from the concrete jungle to the gated paradise of Bel-Air.
 
-  Now, Bel-Air ain''t Philly. It''s mansions and manicured lawns, fancy cars and caviar breakfasts. 
-  My cousins, Carlton and Hilary, were like polished ponies, all rules and etiquette, while I was a dust bunny, 
-  tracking grime across their white carpets. We clashed like cymbals and a kazoo, me with my slang and street smarts, 
-  them with their stuffy manners and opera nights. But hey, opposites attract, right? We learned from each other, 
+  Now, Bel-Air ain''t Philly. It''s mansions and manicured lawns, fancy cars and caviar breakfasts.
+  My cousins, Carlton and Hilary, were like polished ponies, all rules and etiquette, while I was a dust bunny,
+  tracking grime across their white carpets. We clashed like cymbals and a kazoo, me with my slang and street smarts,
+  them with their stuffy manners and opera nights. But hey, opposites attract, right? We learned from each other,
   Carlton loosening up a bit, me learning to hold a fork without stabbing the tablecloth.
 
-  Bel-Air wasn''t Philly, but it became my home. I learned to play polo on the polo field, mastered the art of the perfect topiary trim, 
-  and even impressed Aunt Vivian with my freestyle rap skills at her charity gala. Sure, there were bumps along the way, 
-  culture clashes and misunderstandings, but we were a family, a motley crew of misfits who found harmony in the most unexpected places. 
-  And maybe, just maybe, this fresh prince of Bel-Air had found his kingdom, not in a swanky mansion, 
+  Bel-Air wasn''t Philly, but it became my home. I learned to play polo on the polo field, mastered the art of the perfect topiary trim,
+  and even impressed Aunt Vivian with my freestyle rap skills at her charity gala. Sure, there were bumps along the way,
+  culture clashes and misunderstandings, but we were a family, a motley crew of misfits who found harmony in the most unexpected places.
+  And maybe, just maybe, this fresh prince of Bel-Air had found his kingdom, not in a swanky mansion,
   but in the hearts of the people who filled it with laughter, love, and the occasional rap battle.
 
-  In the end, it wasn''t about the fancy cars or the caviar breakfasts. It was about finding your place, your tribe, 
-  your own version of Bel-Air. And somewhere, between the manicured lawns and the concrete jungle, I found mine.', 
-  true,
-  1
-  );
-
-  (id, title, content, completed, users_id) 
-  VALUES (
-  7, 
-  'The Case of the Crumb Caper: One Duck Detective''s Quacking Adventure', 
-  'Beatrice Beefeater, duck detective extraordinaire, wasn''t your average puddle pouncer. 
-  While her flock waddled about, blissfully unaware of the pond''s underbelly of fishy secrets, 
-  Beatrice yearned for a life of trenchcoats (well, repurposed tea cozies) and magnifying glasses 
-  (fashioned from a discarded contact lens). Her dreams were filled with shadowy figures, stolen worms, 
+  In the end, it wasn''t about the fancy cars or the caviar breakfasts. It was about finding your place, your tribe,
+  your own version of Bel-Air. And somewhere, between the manicured lawns and the concrete jungle, I found mine.', TRUE, 1),
+(7, 'The Case of the Crumb Caper: One Duck Detective''s Quacking Adventure', 'Beatrice Beefeater, duck detective extraordinaire, wasn''t your average puddle pouncer.
+  While her flock waddled about, blissfully unaware of the pond''s underbelly of fishy secrets,
+  Beatrice yearned for a life of trenchcoats (well, repurposed tea cozies) and magnifying glasses
+  (fashioned from a discarded contact lens). Her dreams were filled with shadowy figures, stolen worms,
   and quacking pronouncements of "The case is cracked!"
 
-  One sunny afternoon, a frantic mallard waddled up, beak trembling. His precious stash of breadcrumbs, 
-  meticulously collected from discarded sandwiches, had vanished! Beatrice, her ducky antennae twitching with intrigue, 
-  donned her tea cozy trenchcoat and grabbed her contact lens monocle. "Fear not, fowl friend," she declared, 
+  One sunny afternoon, a frantic mallard waddled up, beak trembling. His precious stash of breadcrumbs,
+  meticulously collected from discarded sandwiches, had vanished! Beatrice, her ducky antennae twitching with intrigue,
+  donned her tea cozy trenchcoat and grabbed her contact lens monocle. "Fear not, fowl friend," she declared,
   puffing out her chest (which mostly just ruffled her feathers). "Beatrice Beefeater is on the case!"
 
-  Her investigation was, well, unique. She interrogated grumpy catfish, 
-  who mumbled through gulps of algae about suspicious bread crumbs floating suspiciously by their lair. 
-  She followed a trail of crumbs (which ended abruptly in a hungry duckling''s belly) and examined footprints 
+  Her investigation was, well, unique. She interrogated grumpy catfish,
+  who mumbled through gulps of algae about suspicious bread crumbs floating suspiciously by their lair.
+  She followed a trail of crumbs (which ended abruptly in a hungry duckling''s belly) and examined footprints
   (mostly just webbed imprints leading in every direction).
 
-  Finally, Beatrice stubbed her foot (again) on a discarded baguette crust. Underneath, a beady eye peered out. 
-  It was Humphrey, the pond bully, notorious for his insatiable appetite and questionable table manners. 
+  Finally, Beatrice stubbed her foot (again) on a discarded baguette crust. Underneath, a beady eye peered out.
+  It was Humphrey, the pond bully, notorious for his insatiable appetite and questionable table manners.
   He confessed to the breadcrumb heist, pleading temporary insanity brought on by too much pond scum.
 
-  Beatrice, magnanimous even in the face of stolen snacks, declared the case closed. 
-  Humphrey, in a rare moment of generosity, offered Beatrice a particularly juicy worm as a reward. 
-  As she savored her detective''s fee, Beatrice couldn''t help but think, maybe duck detective wasn''t 
-  all glamorous trenchcoats and dramatic reveals. But then again, who could resist a hero who solved crimes 
-  with a contact lens monocle and a penchant for tripping over crusty bread? In the wacky world of the pond, 
-  Beatrice Beefeater, the clumsiest, most breadcrumb-obsessed detective around, was just the duck for the job.', 
-  true,
-  3
-  );
+  Beatrice, magnanimous even in the face of stolen snacks, declared the case closed.
+  Humphrey, in a rare moment of generosity, offered Beatrice a particularly juicy worm as a reward.
+  As she savored her detective''s fee, Beatrice couldn''t help but think, maybe duck detective wasn''t
+  all glamorous trenchcoats and dramatic reveals. But then again, who could resist a hero who solved crimes
+  with a contact lens monocle and a penchant for tripping over crusty bread? In the wacky world of the pond,
+  Beatrice Beefeater, the clumsiest, most breadcrumb-obsessed detective around, was just the duck for the job.', TRUE, 3);
