5f888239e645b04ba4c6ee9a95605ef5206efd6b
Patrick Aucoin <aucoinp640@gmail.com>
2024-01-12 23:35:36 -0500

    fixing submission errors

--- db/queries/acceptSubmission.js
+++ db/queries/acceptSubmission.js
@@ -2,8 +2,11 @@ const db = require('../connection');
 
 const acceptSubmission = async (submissionId) => {
   try {
+    const result = await db.query(`SELECT stories_id FROM submissions WHERE id = $1`, [submissionId]);
+    const stories_Id = result.rows[0].stories_id;
+    
     await db.query(`UPDATE submissions SET accepted = true WHERE id = $1`, [submissionId]);
-    await db.query(`UPDATE submissions SET active = false WHERE id != $1`, [submissionId]);
+    await db.query(`UPDATE submissions SET active = false WHERE stories_id = $1 AND id != $2`, [stories_Id, submissionId]);
   } catch (err) {
     throw err;
   }
